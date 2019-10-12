2fc50ff69b6666f819f49fe1a00b82c52e62169c
Hollis Lehv <hlehv@aol.com>
2019-10-12 18:22:41 -0400

    Removed white background from parasite, adding bobbing up and down

---
+++ game/Assets/Scripts/Float.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Float : MonoBehaviour
+{
+    float originalY;
+
+    public float floatStrength;
+    public float floatSpeed;
+
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + ((float)Math.Sin(Time.time * floatSpeed) * floatStrength),
+            transform.position.z);
+    }
+}
