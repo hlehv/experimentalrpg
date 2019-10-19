2dafcf431ec59e5ddbb7733ade0a6a7dfa1de494
yiwengao <yiwen.gao@columbia.edu>
2019-10-18 23:27:18 -0400

    Added overworld

---
+++ game/Assets/Scripts/Dungeon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour
+{
+
+	public List<Dungeon> neighbors;
+	public bool isLocked;
+	public Sprite unlockedDungeon;
+	public Sprite lockedDungeon;
+	public SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocked) sr.sprite = lockedDungeon;
+        else sr.sprite = unlockedDungeon;
+    }
+}
