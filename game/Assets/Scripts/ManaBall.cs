9ed31d6d7218e6250d80d7c3dccc854acd846afd
Hollis Lehv <hlehv@aol.com>
2019-11-22 21:24:33 -0500

    Added mana ball logic

---
+++ game/Assets/Scripts/ManaBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaBall : MonoBehaviour
+{
+    public bool isActive;
+    public Sprite activeSprite;
+    public Sprite inactiveSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive) {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = activeSprite;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = inactiveSprite;
+        }
+
+    }
+}
